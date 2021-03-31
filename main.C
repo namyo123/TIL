4cc6f2f0a16f65cf5d834942cc354aefb4299197
namyo123 <yosebnam@gmail.com>
2021-03-31 19:58:11 +0900

    good haker and musle man

---
+++ main.C
@@ -0,0 +1,17 @@
+#pragma warning (disable : 4996)
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d", &n);
+
+   for (int i = n; i > 0; i--)
+	{
+		
+		for (int j = 0; j <= i - 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+}
